69b63e7a014020c29a2f893184ea82671b91973d
Dalmas Ogembo <dalmasogembo@gmail.com>
2024-12-09 15:30:36 +0300

    Update docusaurus settings to handle build with broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: 'inblockio', // Usually your GitHub org/user name.
   projectName: 'AQUA PKC', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
