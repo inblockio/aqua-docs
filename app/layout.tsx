5b157121e7e4ded8fc53dd459d5cce0d087754b8
Arthur_kamau <kennkamau09@gmail.com>
2025-12-17 17:02:45 +0300

    improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import { getConfig } from "@/lib/config"
 import "./globals.css"
-
+ 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
